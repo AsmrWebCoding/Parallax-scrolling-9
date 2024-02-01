5b18374dd8857b248a789a0bce82b216a88a1f09
Asmr web coding <138141838+AsmrWebCoding@users.noreply.github.com>
2024-02-01 20:02:18 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let text = document.getElementById('text');
+let header = document.getElementById('header');
+let mountain1 = document.getElementById('mountain1');
+let mountain2 = document.getElementById('mountain2');
